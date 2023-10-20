d8e6fd530811ee10c2d73766852527141d226e02
Kitesalet <104630744+Kitesalet@users.noreply.github.com>
2023-10-20 10:13:57 -0300

    Se termino con los deposits y extractions y se coienza con la logica e implementacion de las transferencias entre cuentas

---
+++ SofttekWalletFront/wwwroot/js/Transactions/Transactions.js
@@ -0,0 +1,20 @@
+﻿var token = localStorage.getItem('token');
+
+var id = localStorage.getItem('id');
+
+
+
+$(document).ready(function () {
+
+    $('#transferButton').click(function () {
+
+        var confirmed = confirm("Do you want to complete the transfer?");
+
+        if (confirmed == true) {
+            $('form').submit();
+        }
+    })
+
+})
+
+
